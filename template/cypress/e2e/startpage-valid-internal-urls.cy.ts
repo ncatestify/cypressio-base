d81ce95a95871573f0a49d3a18ce0cffac212d2d
Roland <rolandgolla@gmail.com>
2022-12-29 14:49:45 +0100

    Use new prefix and add google fonts test

--- template/cypress/e2e/startpage-valid-internal-urls.cy.ts
+++ template/cypress/e2e/startpage-valid-internal-urls.cy.ts
@@ -5,14 +5,18 @@ describe('TESTIFY base tests', () => {
   });
 
   it('Imprint link on starpage is clickable', () => {
-    cy.ncaValidateImprintClickable();
+    cy.ttValidateImprintClickable();
+  });
+
+  it('No external google fonts are loading', () => {
+    cy.ttValidateNoGoogleFonts();
   });
 
   it('Every internal page on startpage has status code 200', () => {
-    cy.ncaEveryInternalLinkStatusOk();
+    cy.ttEveryInternalLinkStatusOk();
   });
 
   it('Every internal page on startpage is loading', () => {
-    cy.ncaEveryInternalLinkIsLoading();
+    cy.ttEveryInternalLinkIsLoading();
   });
 });
