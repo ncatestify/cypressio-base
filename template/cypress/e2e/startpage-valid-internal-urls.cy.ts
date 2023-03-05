13260eac60f08630e987fe8ae277cca902ff69d3
Roland <rolandgolla@gmail.com>
2023-03-05 22:13:27 +0100

    Use tt base tests and update to plugin v 16

--- template/cypress/e2e/startpage-valid-internal-urls.cy.ts
+++ template/cypress/e2e/startpage-valid-internal-urls.cy.ts
@@ -5,18 +5,7 @@ describe('TESTIFY base tests', () => {
   });
 
   it('Imprint link on starpage is clickable', () => {
-    cy.ttValidateImprintClickable();
+    cy.ttRunTestifyBaseTests();
   });
 
-  it('No external google fonts are loading', () => {
-    cy.ttValidateNoGoogleServices();
-  });
-
-  it('Every internal page on startpage has status code 200', () => {
-    cy.ttEveryInternalLinkStatusOk();
-  });
-
-  it('Every internal page on startpage is loading', () => {
-    cy.ttEveryInternalLinkIsLoading();
-  });
 });
